32ea9875819b97ad739acdaa60802b01f94785d2
Sujal Visavaliya <150358295+fsd-sujal@users.noreply.github.com>
2023-11-27 02:12:39 -0800

    Add files via upload

---
+++ ifelse.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+main()
+
+{
+	int a,b;
+	printf("enter the value of A:");
+	scanf("%d",&a);
+	printf("enter the value of B:");
+	scanf("%d",&b);
+	
+	if(a< b)
+	{
+		printf("a is minimum");
+	}
+	else{
+		printf("b is minimum");
+	}
+}
---
+++ ladderif.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+main()
+
+{
+	float number;
+	printf("enter the Number:");
+	scanf("%d",&number);
+	if(number==0)
+	{
+		printf("This Number Is Neutral");
+	}
+	else if(number>0){
+		printf("This Number Is Not Neutral");
+	}
+	else{
+		printf("This Number Is Not Neutral");
+	}
+}
